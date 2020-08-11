84cf5ffb8f40f3e907384c3fd76eccb1fc0918d7
alan <alanarostegui@hotmail.com>
2020-08-10 22:34:32 -0500

    refactor(app): main logic, fetch worlwide & by country

--- src/App.js
+++ src/App.js
@@ -1,22 +1,98 @@
-import React from "react";
-import { Card, CardContent } from "@material-ui/core";
-import Header from "./components/header/Header";
+import React, { useState, useEffect } from "react";
+import {
+  FormControl,
+  Select,
+  MenuItem,
+  Card,
+  CardContent,
+} from "@material-ui/core";
+import {
+  apiAll,
+  fetchCountries,
+  apiCountryCode,
+  fetchCountryDataByCode,
+  fetchAllCountryData,
+} from "./api";
+import { formatCountries } from "./helpers";
 import InfoBox from "./components/infoBox/InfoBox";
 import Map from "./components/map/Map";
 
 import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
+
+  useEffect(() => {
+    const getWorldwideData = async () => {
+      const data = await fetchAllCountryData();
+      setCountryInfo(data);
+    };
+
+    getWorldwideData();
+  }, []);
+
+  useEffect(() => {
+    const getCountries = async () => {
+      const data = await fetchCountries();
+      const countries = formatCountries(data);
+      setCountries(countries);
+    };
+
+    getCountries();
+  }, []);
+
+  const onCountryChange = async (e) => {
+    const countryCode = e.target.value;
+
+    const data = await fetchCountryDataByCode(countryCode);
+    setCountry(countryCode);
+
+    setCountryInfo(data);
+
+    /*     const url = countryCode === 'worldwide'
+      ? apiAll
+      : apiCountryCode(countryCode)
+    
+      await  */
+  };
+
+  console.log("ssss", countryInfo);
   return (
     <div className="app">
       <div className="app__left">
         <div className="app__header">
-          <Header />
+          <h1>COVID-19 Stats</h1>
+          <FormControl className="header__dropdown">
+            <Select
+              variant="outlined"
+              onChange={onCountryChange}
+              value={country}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
         </div>
         <div className="app__stats">
-          <InfoBox title="Coronavirus cases" cases={1} total={10} />
-          <InfoBox title="Recovered" cases={1} total={10} />
-          <InfoBox title="Deaths" cases={1} total={10} />
+          <InfoBox
+            title="Coronavirus cases"
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
+          <InfoBox
+            title="Recovered"
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
+          <InfoBox
+            title="Deaths"
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
         </div>
         <Map />
       </div>
--- src/api.js
+++ src/api.js
@@ -1,8 +1,35 @@
 export const api = "https://disease.sh/v3/covid-19/countries";
+export const apiAll = "https://disease.sh/v3/covid-19/all";
+
+export const apiCountryCode = (countryCode) =>
+  `https://disease.sh/v3/covid-19/countries/${countryCode}`;
 
 export const fetchCountries = async () => {
   try {
     const response = await fetch(api);
+    const countries = await response.json();
+    return countries;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
+
+export const fetchCountryDataByCode = async (countryCode) => {
+  try {
+    const url =
+      countryCode === "worldwide" ? apiAll : apiCountryCode(countryCode);
+    const response = await fetch(url);
+    const country = await response.json();
+    return country;
+  } catch (err) {
+    return err;
+  }
+};
+
+export const fetchAllCountryData = async (api) => {
+  try {
+    const response = await fetch(apiAll);
     const countries = await response.json();
     return countries;
   } catch (err) {
