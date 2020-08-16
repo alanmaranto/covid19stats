ad686b1c009c714effcd00a74f8e202053350c38
alan <alanarostegui@hotmail.com>
2020-08-16 18:37:06 -0500

    style(infobox):  change color in letters

--- src/components/infoBox/InfoBox.jsx
+++ src/components/infoBox/InfoBox.jsx
@@ -6,11 +6,11 @@ const InfoBox = ({ title, cases, total }) => {
   return (
     <Card className="infoBox">
       <CardContent>
-        <Typography className="infoBox__title" color="textSecondary">
+        <Typography color="textSecondary">
           {title}
         </Typography>
         <h2 className="infoBox__cases">{cases}</h2>
-        <Typography color="textSecondary">{total} Total</Typography>
+        <Typography className="infoBox__total" color="textSecondary">{total} Total</Typography>
       </CardContent>
     </Card>
   );
